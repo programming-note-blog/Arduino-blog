825dd8d26fbe2cac0cc042ef3d8914450777a8df
DESKTOP-26PKT0I\yamak <yamaken0123456789@gmail.com>
2023-07-31 16:11:18 +0900

    echoを実装

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,12 +1,43 @@
 #define BIT_RATE 9600
+#define READ_BUFFER_SIZE 64
+
+#define IS_TERMINATION(c) ((c) == '\r' || (c) == '\n' || (c) == '\0')
+
+static char ReadBuffer[READ_BUFFER_SIZE];
+static char* pReadBufferEnd;
 
 void setup()
 {
+  pReadBufferEnd = &ReadBuffer[0];
+
   Serial.begin(BIT_RATE);
   Serial.println("Hello World");
 }
 
 void loop()
 {
-  
+  if(Serial.available() <= 0)
+  {
+    return;
+  }
+
+  if(pReadBufferEnd == &ReadBuffer[READ_BUFFER_SIZE])
+  {
+    ReadBuffer[READ_BUFFER_SIZE - 1] = '\0';
+  }
+  else
+  {
+    *pReadBufferEnd = (char)Serial.read();
+  }
+
+  if(IS_TERMINATION(*pReadBufferEnd))
+  {
+    *pReadBufferEnd = '\0';
+    Serial.println(ReadBuffer);
+    pReadBufferEnd = &ReadBuffer[0];
+
+    return;
+  }
+
+  pReadBufferEnd++;
 }
