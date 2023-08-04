d9ba730670a9412881aec1ca24499f39ba7343f5
DESKTOP-26PKT0I\yamak <yamaken0123456789@gmail.com>
2023-08-04 21:02:52 +0900

    Commandを実装

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,10 +1,153 @@
+#ifdef SUCCESS
+#undef SUCCESS
+#endif
+#define SUCCESS 0
+#ifdef ERROR
+#undef ERROR
+#endif
+#define ERROR   1
+
 #define BIT_RATE 9600
 #define READ_BUFFER_SIZE 64
+#define ARGC_MAX 8
 
 #define IS_TERMINATION(c) ((c) == '\r' || (c) == '\n' || (c) == '\0')
+#define IS_SPACE(c)       ((c) ==' ')
+#define IS_ERROR(code)    ((code) != SUCCESS)
 
 static char ReadBuffer[READ_BUFFER_SIZE];
 static char* pReadBufferEnd;
+static unsigned short ArgC;
+static char* ArgV[ARGC_MAX];
+
+static void execute_command(unsigned short argc, char** argv);
+static unsigned short cmd_echo(unsigned short argc, char** argv);
+static unsigned short cmd_dw(unsigned short argc, char** argv);
+
+static unsigned short get_arg(char* line, char** argv);
+
+typedef struct tCommand
+{
+  const char* key;                                                // キーとなる単語
+  unsigned short (*func)(unsigned short argc, char** argv);       // 呼び出す関数
+  const char* usage;                                              // 使い方
+} SCommand;
+
+static const SCommand COMMAND_TABLE[] =                           // コマンドテーブル
+{
+  {"echo", cmd_echo, "echo <prm>            :display prm"},
+  {"dw",   cmd_dw,   "dw <pin> <value(0/1)> :digitalWrite(pin,value)"},
+};
+
+static unsigned short cmd_echo(unsigned short argc, char** argv)
+{
+  unsigned short i;
+
+  if(argc == 0)
+  {
+    return ERROR;
+  }
+
+  Serial.print(argv[0]);
+
+  for(i = 1; i < argc; i++)
+  {
+    Serial.print(" ");
+    Serial.print(argv[i]);
+  }
+  Serial.println();
+
+  return SUCCESS;
+}
+
+static unsigned short cmd_dw(unsigned short argc, char** argv)
+{
+  unsigned short pin_num;
+  unsigned short value;
+
+  if(argc < 2)
+  {
+    return ERROR;
+  }
+
+  pin_num = atoi(argv[0]);
+  value = atoi(argv[1]);
+
+  pinMode(pin_num, OUTPUT);
+  digitalWrite(pin_num, value);
+
+  return SUCCESS;
+}
+
+static unsigned short get_arg(char* line, char** argv)
+{
+  unsigned short i;
+  unsigned short argc = 0;
+
+  // 1文字目
+  if(!IS_SPACE(line[0]) && !IS_TERMINATION(line[0]))
+  {
+    // 「スペースでも終端文字でもない」ので「単語の先頭の文字」
+    argv[argc] = &line[0];
+    argc++;
+  }
+
+  // 2文字目以降
+  for(i = 1; i < READ_BUFFER_SIZE; i++)
+  {
+    if(IS_TERMINATION(line[i]))
+    {
+      line[i] = '\0';
+      return argc;
+    }
+
+    if(IS_SPACE(line[i]))
+    {
+      // 単語ごとに分割するのでスペースは終端文字に上書きする
+      line[i] = '\0';
+      continue;
+    }
+
+    if(IS_TERMINATION(line[i - 1]))
+    {
+      // 「スペースでも終端文字でもない かつ ひとつ前の文字が終端文字」ということは「単語の先頭の文字」
+      argv[argc] = &line[i];
+      argc++; 
+
+      if(argc == ARGC_MAX)
+      {
+        return ARGC_MAX - 1;
+      }
+    }
+  }
+
+  Serial.println("[error]line without end");
+  return 0;
+}
+
+static void execute_command(unsigned short argc, char** argv)
+{
+  unsigned short i;
+  unsigned short result;
+
+  for(i = 0; i < sizeof(COMMAND_TABLE) / sizeof(SCommand); i++)
+  {
+    if(!strcmp(COMMAND_TABLE[i].key, argv[0]))
+    {
+      result = COMMAND_TABLE[i].func(argc - 1, &argv[1]);
+      if(IS_ERROR(result))
+      {
+        Serial.println(COMMAND_TABLE[i].usage);
+      }
+      return;
+    }
+  }
+
+  for(i = 0; i < sizeof(COMMAND_TABLE) / sizeof(SCommand); i++)
+  {
+    Serial.println(COMMAND_TABLE[i].usage);
+  }
+}
 
 void setup()
 {
@@ -12,6 +155,8 @@ void setup()
 
   Serial.begin(BIT_RATE);
   Serial.println("Hello World");
+  
+  Serial.print("> ");
 }
 
 void loop()
@@ -32,9 +177,16 @@ void loop()
 
   if(IS_TERMINATION(*pReadBufferEnd))
   {
+    Serial.println();
+    
     *pReadBufferEnd = '\0';
-    Serial.println(ReadBuffer);
+
+    ArgC = get_arg(&ReadBuffer[0], ArgV);
+    execute_command(ArgC, ArgV);
+    
     pReadBufferEnd = &ReadBuffer[0];
+
+    Serial.print("> ");
 
     return;
   }
