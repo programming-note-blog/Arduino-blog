d4d51c9d1558c5efea41b836a35891614013bc15
ymken <programming.note.blog@gmail.com>
2024-12-14 23:03:23 +0900

    モーター制御を追加

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -3,6 +3,7 @@
 #include "serial.h"
 #include "pin_control.h"
 #include "button_control.h"
+#include "motor_control.h"
 
 // ボタンが押されたときの処理(仮)
 void onButtonPress() {
@@ -15,6 +16,8 @@ void setup()
 
   // ボタン用ピンの初期化（7番ピンにコールバックを登録）
   ButtonInit(7, onButtonPress);
+
+  MotorControlInit();
 }
 
 void loop()
---
+++ arduino/motor_control.h
@@ -0,0 +1,10 @@
+#ifndef MOTOR_CONTROL_H
+#define MOTOR_CONTROL_H
+
+unsigned short MotorControlInit();
+unsigned short MotorControlSetRightMotorSpeed(short speed);
+unsigned short MotorControlSetLeftMotorSpeed(short speed);
+unsigned short MotorControlStopRightMotor();
+unsigned short MotorControlStopLeftMotor();
+
+#endif // MOTOR_CONTROL_H
